422250d460716b138c460201704977e831afea9a
Parithoshkp <kesturparithosh@gmail.com>
2023-08-20 17:48:18 +0530

    date-counter initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./app.css";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  date.setDate(date.getDate() + count);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <p>Step is: {step}</p>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div className="container">
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <p>Count is: {count}</p>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <div className="container">
+        {count === 0 ? (
+          <p>today is {date.toDateString()}</p>
+        ) : count > 0 ? (
+          <p>
+            {count} days from today its {date.toDateString()}
+          </p>
+        ) : (
+          <p>
+            {-1 * count} days ago it was {date.toDateString()}
+          </p>
+        )}
+      </div>
+    </>
   );
 }
 
