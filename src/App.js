fc3b4694b03433021879a61779e7b4b942ab26de
Parithoshkp <kesturparithosh@gmail.com>
2023-08-24 11:54:19 +0530

    date-counter with slider

--- src/App.js
+++ src/App.js
@@ -6,16 +6,30 @@ function App() {
   const [count, setCount] = useState(0);
   const date = new Date();
   date.setDate(date.getDate() + count);
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
   return (
     <>
       <div className="container">
-        <button onClick={() => setStep((s) => s - 1)}>-</button>
-        <p>Step is: {step}</p>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
+        <span>Step</span>
+        <input
+          type="range"
+          min={1}
+          max={10}
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        ></input>
+        <span>{step}</span>
       </div>
       <div className="container">
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <p>Count is: {count}</p>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        ></input>
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <div className="container">
@@ -30,6 +44,9 @@ function App() {
             {-1 * count} days ago it was {date.toDateString()}
           </p>
         )}
+      </div>
+      <div className="container">
+        {count > 0 ? <button onClick={handleReset}>Reset</button> : null}
       </div>
     </>
   );
